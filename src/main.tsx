8065fbb0527147aef4a05aae1c43ef1497744904
Charles Perng <charles@perng.com>
2024-11-29 11:23:40 -0800

    add info icon text

--- src/main.tsx
+++ src/main.tsx
@@ -53,7 +53,7 @@ function InfoPopup() {
         }}
       >
         <Typography sx={{ p: 2 }}>
-          此英語學習遊戲正在開發中，歡迎您試用
+        此英語學習遊戲正在開發中，歡迎您試用，在開發期間，使用者介面會不斷修改，英語資料也可能有錯誤，還請見諒，如果您發現有任何錯誤或有任何建議，請
           <br />
           <Link href="https://forms.gle/AuKcZyUYvtdsDuFH6" target="_blank" rel="noopener">
             提供意見回饋
